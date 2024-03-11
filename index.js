46440f98e9a7c8983fefd90822c93dd21eb2208a
Kingsley Huang <kingsley.huang@mycare.org.nz>
2024-03-11 18:38:50 +1300

    init commit

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log(1/0) // division by zero = Infinity
+console.log(typeof((-1/0))) // negative division by zero = -Infinity
+console.log(typeof("not a number" / 1)) // invalid mathematical operation = NaN
